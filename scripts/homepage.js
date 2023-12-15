a1d6c9647005ff19e710aa8e6f3e3fd3519bcef4
anurag singh <anurag6569201@gmail.com>
2023-12-15 15:08:58 +0530

    added animation

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,8 +1,9 @@
 let navbar=document.getElementById("navbar");
 let hero_text=document.querySelector(".hero_text p");
 let content_head=document.querySelector(".content_head");
+let redeem_head=document.querySelector(".redeem_head");
 window.addEventListener("scroll", () => {
-    const offset = window.pageYOffset;
+    const offset = window.pageYOffset*0.7;
     console.log(offset);
     if(offset>=10){
         navbar.style.backdropFilter = "blur(10px)";
@@ -14,4 +15,29 @@ window.addEventListener("scroll", () => {
     }
     // if(offset>=)
     // parllel.style.backgroundPositionY = offset * 0.7 + "px";
+
+});
+
+//   text revealing js
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 85%",
+    end: "bottom 15%",
+    markers: false,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    }
+  });
 });
