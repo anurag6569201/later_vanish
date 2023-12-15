0f51bb964969a76c8699e1e868289ce6f1f5912a
anurag singh <anurag6569201@gmail.com>
2023-12-15 11:26:38 +0530

    added js for some animation

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,11 +1,17 @@
 let navbar=document.getElementById("navbar");
+let hero_text=document.querySelector(".hero_text p");
+let content_head=document.querySelector(".content_head");
 window.addEventListener("scroll", () => {
     const offset = window.pageYOffset;
+    console.log(offset);
     if(offset>=10){
         navbar.style.backdropFilter = "blur(10px)";
+        hero_text.style.opacity="1";
     }
     else{
         navbar.style.backdropFilter = "blur(0px)";
+        hero_text.style.opacity="0";
     }
+    // if(offset>=)
     // parllel.style.backgroundPositionY = offset * 0.7 + "px";
 });
