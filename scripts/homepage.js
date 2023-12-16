b9924e4c7f2a8299945923b626297e6d3dfcb20b
anurag singh <anurag6569201@gmail.com>
2023-12-16 14:47:59 +0530

    added navbar responsive

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -63,5 +63,5 @@ const loader = document.getElementById("loader");
 window.addEventListener('load', () => {
     setTimeout(() => {
         loader.style.display = "none";
-    }, 2000);
+    },2000);
 });
